7bce45d0f7458404c08ee32ee853ecbf700decaa
Alex Monnet <alexcmonnet@gmail.com>
2021-11-21 23:19:23 +0000

    Fleshed out some fuctionality

--- src/coffee/cli.py
+++ src/coffee/cli.py
@@ -1,4 +1,23 @@
+import argparse
+import coffee.coffee
 
 def run_cli():
-    print("Cli")
-    return
+    arg_parser = setup_argparser()
+    args = arg_parser.parse_args()
+    args_dict = vars(args)
+
+    interval_time = args_dict.get("interval") or 300
+
+    coffee.keep_alive()
+   
+    
+
+def setup_argparser():
+
+    parser = argparse.ArgumentParser(description='Keep your computer awake.')
+    parser.add_argument('--interval', type=int, default=max,
+                    help='Number of seconds between keep-alive actions')
+
+    args = parser.parse_args()
+
+    return parser
--- src/coffee/coffee.py
+++ src/coffee/coffee.py
@@ -1,10 +1,11 @@
 import time
 
-from coffee.interactions import 
+from coffee.interactions import enter_input
 
 def keep_alive(time_between_actions_in_seconds: int = 300, perform_n_times: int = 99999999):
 
     for i in range(perform_n_times):
+        print("wiggle")
         enter_input("numlock")
         enter_input("numlock")
         time.sleep(time_between_actions_in_seconds)    
